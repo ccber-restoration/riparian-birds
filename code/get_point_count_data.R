bb56129799c3ffeddd80568a05c0608fb2b8d8ec
fjoyce <fhjoyce@gmail.com>
2025-06-27 11:25:44 -0700

    start filtering by focal species & summarizing detections

--- code/get_point_count_data.R
+++ code/get_point_count_data.R
@@ -1,20 +1,8 @@
-#packages
 
-library(tidyverse)
-
-#data cleaning
-#see https://cran.r-project.org/web/packages/janitor/vignettes/janitor.html#do-those-dataframes-actually-contain-the-same-columns
-library(janitor)
-
-# import data from Google Sheets
-library(googlesheets4)
-
-#deal with dates and times
-library(lubridate)
-library(hms)
+#source script to load packages
+source("code/0_libraries.R")
 
 #read in individual sheets (1 per day)
-
 
 Ellwood_2025_06_18 <- read_sheet("https://docs.google.com/spreadsheets/d/1QVSkGbZxlq0ZbWmu40SsNY4cnZfR5376Xe0QS0HW9Yw/edit?gid=0#gid=0")  
 
@@ -33,4 +21,22 @@ point_counts_all <- bind_rows(Ellwood_2025_06_18,
   mutate(time_start = as_hms((time_start)),
          time_stop = as_hms(time_stop))
 
+#list of focal species 
+focal_sp <- c("Yellow Warbler", "Wilson's Warbler", "Warbling Vireo", "Black-headed Grosbeak", "Purple Finch", "Chestnut-backed Chickadee", "Western Flycatcher")
 
+
+#read in AOS North America checklist and filter to focal species
+#this ensures they are listed in taxonomic order and gives more taxonomic information (family, scientific name)
+
+aos_checklist <- read_csv("data/NACC_list_species_2024.csv") %>% 
+  filter(common_name %in% focal_sp)
+
+
+#filter by focal species
+point_counts_filtered <- point_counts_all %>% 
+  filter(species %in% aos_checklist$common_name)
+
+#summarize 
+point_count_summary <- point_counts_filtered %>% 
+  group_by(species, site) %>% 
+  summarize(n_detections = n())
