c62ff9251998382b86c07db31402ab6047f35ca6
fjoyce <fhjoyce@gmail.com>
2025-06-27 11:25:18 -0700

    start moving code into setup script

---
+++ code/0_libraries.R
@@ -0,0 +1,14 @@
+#packages
+
+library(tidyverse)
+
+#data cleaning
+#see https://cran.r-project.org/web/packages/janitor/vignettes/janitor.html#do-those-dataframes-actually-contain-the-same-columns
+library(janitor)
+
+# import data from Google Sheets
+library(googlesheets4)
+
+#deal with dates and times
+library(lubridate)
+library(hms)
