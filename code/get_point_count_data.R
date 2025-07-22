0c392206fca803cd194256de2d2d8e8d415810c6
fjoyce <fhjoyce@gmail.com>
2025-07-22 09:39:03 -0700

    better organization, more filling, pull in all counts through round 4

--- code/get_point_count_data.R
+++ code/get_point_count_data.R
@@ -18,35 +18,48 @@ aos_checklist <- read_csv("data/NACC_list_species_2024.csv") %>%
 
 #read in individual sheets (1 per day)
 
+#Round 1 ----
 Ellwood_2025_06_18 <- read_sheet("https://docs.google.com/spreadsheets/d/1QVSkGbZxlq0ZbWmu40SsNY4cnZfR5376Xe0QS0HW9Yw/edit?gid=0#gid=0")  
 
 Atascadero_2025_06_19 <- read_sheet("https://docs.google.com/spreadsheets/d/1W_LAFmjLoMvpnsNEjhi-Pb2xZM8cMdxpNpYjwUS11Vk/edit?gid=0#gid=0")
 
-Ellwood_2025_06_25 <- read_sheet("https://docs.google.com/spreadsheets/d/1J7jfTSaFu-AhboBX8Qlv-WGFrkVNQ92KucSjN34pHo4/edit?gid=0#gid=0")
+#Round 2 ----
+Ellwood_2025_06_25 <- read_sheet("https://docs.google.com/spreadsheets/d/1J7jfTSaFu-AhboBX8Qlv-WGFrkVNQ92KucSjN34pHo4/edit?gid=0#gid=0") %>% 
+  #fill NA values for specific columns based on values above
+  fill(Site, aru_site_name, Date, Surveyor, Time_start, Time_stop) 
 
 Atascadero_2025_06_27 <- read_sheet("https://docs.google.com/spreadsheets/d/1U3xrgwKnzCwHA2i4aa1CwpnDBge7ViizlcsIIby1lOs/edit?gid=0#gid=0") %>% 
   #fill NA values for specific columns based on values above
-  fill(Site, aru_site_name, Date, Time_start, Time_stop) 
+  fill(Site, aru_site_name, Date, Surveyor, Time_start, Time_stop) 
 
+# Round 3 ----
 Atascadero_2025_07_01 <- read_sheet("https://docs.google.com/spreadsheets/d/1XtjiHkkbtpBRlr472N-wo0OtnAyIv-fTR5vPYW5En68/edit?gid=0#gid=0") %>% 
   #fill NA values for specific columns based on values above
-  fill(Site, aru_site_name, Date, Time_start, Time_stop)
+  fill(Site, aru_site_name, Date, Surveyor, Time_start, Time_stop)
 
 Ellwood_2025_07_02 <- read_sheet("https://docs.google.com/spreadsheets/d/12gD8q2Qz3AVibUXeqmKMb1NTxRYsxupaKrcIOO1YqZg/edit?gid=0#gid=0") %>% 
   #fill NA values for specific columns based on values above
-  fill(Site, aru_site_name, Date, Time_start, Time_stop)
+  fill(Site, aru_site_name, Date, Surveyor, Time_start, Time_stop)
 
+#Round 4 ----
 Atascadero_2025_07_15 <- read_sheet("https://docs.google.com/spreadsheets/d/1Eegp06Z-SKHS1-gdJLb7sNjndbThpvNn5xA4WMcemaQ/edit?gid=0#gid=0") %>% 
   #fill NA values for specific columns based on values above
-  fill(Site, aru_site_name, Date, Time_start, Time_stop)
+  fill(Site, aru_site_name, Date, Surveyor, Time_start, Time_stop)
 
+
+Ellwood_2025_07_16 <- read_sheet("https://docs.google.com/spreadsheets/d/16AniPKMXiikSF75Qs0brzaEdFYLper_64KL1R4mAG8s/edit?gid=0#gid=0") %>% 
+  #fill NA values for specific columns based on values above
+  fill(Site, aru_site_name, Date, Surveyor, Time_start, Time_stop)
+  
+  
 point_counts_all <- bind_rows(Ellwood_2025_06_18, 
                               Atascadero_2025_06_19,
                               Ellwood_2025_06_25,
                               Atascadero_2025_06_27,
                               Atascadero_2025_07_01,
                               Ellwood_2025_07_02,
-                              Atascadero_2025_07_15) %>% 
+                              Atascadero_2025_07_15,
+                              Ellwood_2025_07_16) %>% 
   #make column names easier to work with
   clean_names() %>% 
   #extract time of day from the time columns 
