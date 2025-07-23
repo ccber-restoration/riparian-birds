714cfa298d7f0f55fd7f96e1802fd0dee141d09d
fjoyce <fhjoyce@gmail.com>
2025-07-23 11:26:32 -0700

    demo script to view map of sites

--- code/spatial.R
+++ code/spatial.R
@@ -4,6 +4,5 @@ aru_sites <- read_csv(file = "data/aru_sites_2025.csv")
 
 aru_sites_sf <- st_as_sf(aru_sites, 
                          coords=c("x","y"), crs=4326) # remember x=lon and y=lat
-
 mapview(aru_sites_sf)                         
 
