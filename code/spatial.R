7ccfb1666e5e4ea464287f7ab52cf1f1ecac50d3
fjoyce <fhjoyce@gmail.com>
2025-07-31 14:10:34 -0700

    working with mapped trees

--- code/spatial.R
+++ code/spatial.R
@@ -4,5 +4,14 @@ aru_sites <- read_csv(file = "data/aru_sites_2025.csv")
 
 aru_sites_sf <- st_as_sf(aru_sites, 
                          coords=c("x","y"), crs=4326) # remember x=lon and y=lat
-mapview(aru_sites_sf)                         
+mapview(aru_sites_sf)      
 
+#read in Google Sheet with information on trees (species and locations) for arthropod surveys
+survey_trees <- read_sheet("https://docs.google.com/spreadsheets/d/1Srv19FkyvrlMOKrPIIfxB8UuFXuNQoblnqwP8OXjdJU/edit?gid=1506704030#gid=1506704030") %>% 
+  filter(!is.na(latitude)) %>% 
+  filter(!is.na(longitude))
+
+survey_trees_sf <- st_as_sf(survey_trees,
+                            coords = c("latitude", "longitude"), crs=4326)
+
+mapview(survey_trees_sf)
