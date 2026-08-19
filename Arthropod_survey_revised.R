80fa73cbe637eb53333642a96307f8b880ce00cb
santielupi-alt <santi.e.lupi@gmail.com>
2026-08-19 22:11:08 +0000

    revised arthropod figures

---
+++ Arthropod_survey_revised.R
@@ -0,0 +1,154 @@
+# Set up ----
+#source script to load packages
+source("code/survey_trees.R")
+
+# read in data ----
+#read in individual sheets (1 per site and round of surveying)
+
+## round 1 ----
+R1_EW <- read_sheet("https://docs.google.com/spreadsheets/d/11LvIr1BTAlVAseYOcaFbqqcSU8jbIsr6Ugh2mGN4qKg/edit?gid=0#gid=0") %>% 
+  clean_names() %>% 
+  #fill NA values for specific columns based on values above
+  fill(site, date, survey_type, observer, temp_f, site_notes, time, survey_code, leaf_length, number_leaves, herbivory_percent) %>% 
+  #problems caused by data in wrong column
+  mutate(number_leaves = as.numeric(number_leaves),
+         herbivory_percent = as.numeric(herbivory_percent))
+
+R1_AC <- read_sheet("https://docs.google.com/spreadsheets/d/1dcgOch8yOuFYNgQzJMgnAntAETPNBd0L83tfDUcvXjg/edit?gid=0#gid=0") %>% 
+  clean_names() %>% 
+  #fill NA values for specific columns based on values above
+  fill(site, date, survey_type, observer, temp_f, site_notes, time, survey_code, leaf_length, number_leaves, herbivory_percent)
+
+
+## round 2 ----
+R2_AC <- read_sheet("https://docs.google.com/spreadsheets/d/1qdSLQDAS7aV6D8ZUvvYOfGN-D96deh3HJRcLmGx43zk/edit?gid=0#gid=0") %>% 
+  clean_names() %>% 
+  #fill NA values for specific columns based on values above
+  fill(site, date, survey_type, observer, temp_f, site_notes, time, survey_code, leaf_length, number_leaves, herbivory_percent)
+
+R2_EW <- read_sheet("https://docs.google.com/spreadsheets/d/13ycy1crmJ6GM_YHJehRlkFr1fYcOZNQ8DsPWglM-Wi8/edit?usp=drivesdk") %>% 
+  clean_names() %>% 
+  #fill NA values for specific columns based on values above
+  fill(site, date, survey_type, observer, temp_f, site_notes, time, survey_code, leaf_length, number_leaves, herbivory_percent)
+
+
+
+## round 3 ----
+R3_AC <- read_sheet("https://docs.google.com/spreadsheets/d/172N7UQAuOK3_l8qqukiaRqY3EMJMP87m6Pww8y-lW4w/edit?usp=drivesdk") %>% 
+  clean_names() %>% 
+  #fill NA values for specific columns based on values above
+  fill(site, date, survey_type, observer, temp_f, site_notes, time, survey_code, leaf_length, number_leaves, herbivory_percent)
+
+R3_EW <- read_sheet("https://docs.google.com/spreadsheets/d/1b7P7-CEHqYJVjCABV_O2kjwJpxGbbPXiDVTpt3tbpFQ/edit?usp=drivesdk") %>% 
+  clean_names() %>% 
+  #fill NA values for specific columns based on values above
+  fill(site, date, survey_type, observer, temp_f, site_notes, time, survey_code, leaf_length, number_leaves, herbivory_percent)
+
+## round 4 ----
+R4_AC <- read_sheet("https://docs.google.com/spreadsheets/d/1puD2IQhEJ_nsicDhLz2R1UQKRN49-dDBaTTSd4VCzWM/edit?usp=drivesdk") %>% 
+  clean_names() %>% 
+  #fill NA values for specific columns based on values above
+  fill(site, date, survey_type, observer, temp_f, site_notes, time, survey_code, leaf_length, number_leaves, herbivory_percent)
+
+R4_EW <- read_sheet("https://docs.google.com/spreadsheets/d/1pvOXvHncaixcmtX4rDSnO6LXTUkrIkWWw7aHhlfY4V8/edit?usp=drivesdk") %>% 
+  clean_names() %>% 
+  #fill NA values for specific columns based on values above
+  fill(site, date, survey_type, observer, temp_f, site_notes, time, survey_code, leaf_length, number_leaves, herbivory_percent)
+
+## round 5 ----
+R5_AC <- read_sheet("https://docs.google.com/spreadsheets/d/13hW0REjy28RPiVMZW6NT7L4TsS0o2OjZaGuhsTnqyAk/edit?usp=drivesdk") %>% 
+  clean_names() %>% 
+  #fill NA values for specific columns based on values above
+  fill(site, date, survey_type, observer, temp_f, site_notes, time, survey_code, leaf_length, number_leaves, herbivory_percent)
+
+R5_EW <- read_sheet("https://docs.google.com/spreadsheets/d/1i7EHw-oD905TZo1z22S4pe6AX7F2J-oSvLELv5qIAuU/edit?usp=drivesdk") %>% 
+  clean_names() %>% 
+  #fill NA values for specific columns based on values above
+  fill(site, date, survey_type, observer, temp_f, site_notes, time, survey_code, leaf_length, number_leaves, herbivory_percent)
+
+
+#combine data sets ----
+arthropod_surveys <- bind_rows(R1_EW, 
+                               R1_AC,
+                               R2_AC,
+                               R2_EW,
+                               R3_AC,
+                               R3_EW,
+                               R4_AC,
+                               R4_EW,
+                               R5_AC,
+                               R5_EW) %>% 
+  left_join(y = survey_trees, by = join_by(survey_code))
+
+#write to file
+write_csv(arthropod_surveys, "data/arthropod_surveys_compiled.csv")
+
+
+
+#basic summary ----
+arthropod_summary <- arthropod_surveys %>% 
+  #use the group by function to group the data by both site and arthropod category
+  group_by(site, arthropod_common_name) %>% 
+  #use the summarize function to create a new column called total_count, 
+  #which is the sum of the numbers, within each of the site*common name combinations
+  summarize(total_count = sum(number),
+            mean_count = total_count/300) %>% 
+  ungroup()
+
+#things to check:
+#non-integer total counts?
+#NA (blank) values for common name
+#NA values for total count
+#this probably requires updating data validation in individual Google Sheets
+
+
+#summarize by tree species ----
+arthropod_summary_by_tree_sp <- arthropod_surveys %>% 
+  #use the group by function to group the data by both site and arthropod category
+  group_by(species_scientific, arthropod_common_name) %>% 
+  #use the summarize function to create a new column called total_count, 
+  #which is the sum of the numbers, within each of the site*common name combinations
+  summarize(total_count = sum(number)) %>% 
+  ungroup()
+#add native vs. nonnative status to trees 
+#separate beetles into adults vs. larvae
+#plotting herbivory level by tree species 
+#update eucalyptus beetle genus names to paropsisterna 
+
+#plot
+fig_arthropods_by_area_updated <- ggplot(data = arthropod_summary, 
+                                 aes(x = mean_count, y = arthropod_common_name, fill = site)) +
+  geom_col(position = position_dodge()) +
+  xlab("count per 50 leaves") +
+  ylab("Survey area") +
+  #xlab("Survey area") +
+  #ylab("count per 50 leaves") +
+  theme_cowplot() +
+  scale_x_continuous(expand = c(0,NA)) +
+  #scale_fill_manual(values = cal_palette("figmtn")) +
+  guides(fill=guide_legend(title="site")) +
+  #clean up legend
+  theme(
+    legend.key.size = unit(1, "mm"),
+    legend.text = element_text(size = 14),
+    legend.position = "bottom")
+  
+  
+fig_arthropods_by_area_updated
+
+ggsave("figures/Fig_arthropods_by_area_draft.pdf", fig_arthropods_by_area,
+       width = 190, height = 140, units = "mm")
+
+
+unique(arthropod_surveys$aru_site)
+
+unique(arthropod_surveys$aru_site_formatted)
+
+unique(arthropod_surveys$number)
+
+
+# summarize arthropod abundance by ARU ----
+arthropod_aru_summary <- arthropod_surveys %>% 
+  group_by(aru_site_formatted) %>% 
+  summarize(total_arthropods = sum(number, na.rm = TRUE))
+
